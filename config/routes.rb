020ad708ba0ead4e1bc2c88692476fcca1d4aa7a
EC2 Default User <ec2-user@ip-172-31-27-15.ap-northeast-1.compute.internal>
2024-01-16 15:04:53 +0000

    [Finish?] 作成完了？

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+
   get '/' => 'homes#top'
+  root to: 'home#top'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
